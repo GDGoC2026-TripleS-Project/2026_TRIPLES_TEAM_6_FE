e95070b5b5bdfb9bfc7dfc454a5280bedf3088b5
Bae Yujin <ujini7833@gmail.com>
2026-01-26 00:46:27 +0900

    feat: ToggleSwitch 구현 (#7)

--- App.tsx
+++ App.tsx
@@ -1,29 +1,14 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useFonts } from 'expo-font';
-import ListItem from './src/components/common/ListItem';
+import ToggleSwitch from './src/components/common/ToggleSwitch';
 
 export default function App() {
-
-  const [loaded] = useFonts({
-    'Pretendard-Regular': require('./assets/fonts/Pretendard-Regular.otf'),
-    'Pretendard-Medium': require('./assets/fonts/Pretendard-Medium.otf'),
-    'Pretendard-SemiBold': require('./assets/fonts/Pretendard-SemiBold.otf'),
-    'Pretendard-Bold': require('./assets/fonts/Pretendard-Bold.otf'),
-  });
-
-  if (!loaded) return null;
+  const [isOn, setIsOn] = useState(false);
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ width: '100%', marginTop: 100 }}>
-        <ListItem
-        title='메인 텍스트'
-        subtitle='서브 텍스트'
-        onPress={() => console.log('clicked')}
-        />
-      </View>
+      <ToggleSwitch value={isOn} onValueChange={setIsOn} />
     </SafeAreaView>
   );
 }
@@ -32,6 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#0B0B0B',
-    paddingTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
